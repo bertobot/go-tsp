0d5f459d40783241801a27760a64d136e892cb71
Fairfax Smartware <info@fairfaxsmartware.com>
2020-03-04 21:27:52 -0500

    added tests

--- tsp/tsp_test.go
+++ tsp/tsp_test.go
@@ -2,11 +2,26 @@ package tsp
 
 import "testing"
 
+func testpoll() ([]KV, int, string) {
+    var pairs []KV
+    return pairs, 1234567890111, "namespace"
+}
+
 func TestTsp(t *testing.T) {
-    want := "v0.1"
+    want := "v0.2"
 
     if got := Version; got != want {
         t.Errorf("Version = %q, want %q", got, want)
     }
+}
+
+func TestPoller(t *testing.T) {
+    client := NewClient("localhost", 33030)
+    poller := NewPoller(client, 60.0, nil, nil)
+    poller.Add(testpoll)
+    poller.Add(func()([]KV, int, string) {
+        var pairs []KV
+        return pairs, 1234567890222, "key"
+    })
 }
 
--- tsp/version.go
+++ tsp/version.go
@@ -1,3 +1,3 @@
 package tsp
 
-const Version = "v0.1"
+const Version = "v0.2"
